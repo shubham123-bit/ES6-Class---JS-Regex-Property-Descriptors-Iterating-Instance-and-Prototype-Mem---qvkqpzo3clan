86f177d52e49759ff956af01328751129045a236
shubham123-bit <56935378+shubham123-bit@users.noreply.github.com>
2023-10-31 22:24:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-//Write your code here
+class API {
+    #secure
+    constructor(url, method = 'GET') {
+        this.url = url
+        this.method = method
+        this.#secure = url.startsWith("https")
+    }
+
+    isSecure() {
+        return this.#secure
+    }
+    updateUrl(url) {
+        this.url = url
+        this.#secure = url.startsWith("https")
+    }
+}
+
+module.exports = { API }//Write your code here
 
 
 
